2e315cd90596fd665fb246d22fda0f49df0db9df
bmingles <github@emeraldwalk.com>
2023-11-13 00:53:55 +0000

    Deploying to gh-pages from @ emeraldwalk/ewsolid@3cbb559d1756bb1bcbb7c334041a6e33740bbe8b 🚀

---
+++ assets/index-8454fe2e.js
@@ -0,0 +1,1 @@
+import{M as r}from"./index-2505e641.js";import{c as e,m as c}from"./entry-client-c80d9108.js";function o(n){const t=Object.assign({h1:"h1"},r(),n.components);return e(t.h1,{children:"Hello World!"})}function s(n={}){const{wrapper:t}=Object.assign({},r(),n.components);return t?e(t,c(n,{get children(){return e(o,n)}})):o(n)}export{s as default};
